7c472a62a54d84e8c9e65c8b1c27500eaaed9723
Constantinos Ioannidis <kwtsios_15@hotmail.com>
2019-10-19 22:28:23 +0100

    loginControllerTest independent of hibernate

--- src/test/java/com/project/movies/Controller/LoginControllerTest.java
+++ src/test/java/com/project/movies/Controller/LoginControllerTest.java
@@ -3,8 +3,7 @@ package com.project.movies.Controller;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,8 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.containsString;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(LoginController.class)
 public class LoginControllerTest {
 
     @Autowired
